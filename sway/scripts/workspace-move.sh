79212f882a6ff276454d9de7673401eb59cba6fc
Aiden Pickett <Ka7nqk@gmail.com>
2026-08-19 22:43:41 -0600

    Change workspace move script so you can go left if your workspace or the next one has something in it

--- sway/scripts/workspace-move.sh
+++ sway/scripts/workspace-move.sh
@@ -6,7 +6,7 @@ if [ "$1" = "prev" ]; then
     if [ "$current" -gt 1 ]; then
         swaymsg workspace $((current - 1))
     fi
-elif [[ "$1" = "next" ]] && swaymsg -t get_tree | jq --argjson ws "$current" -e '.nodes[].nodes[] | select(.type=="workspace" and .num==$ws) | recurse(.nodes[], .floating_nodes[]) | select(.type=="con" or .type=="floating_con")' > /dev/null; then
-    # Go right, creating windows if not currently in empty workspace
+elif [[ "$1" = "next" ]] && (swaymsg -t get_tree | jq --argjson ws "$current" -e '.nodes[].nodes[] | select(.type=="workspace" and .num==$ws) | recurse(.nodes[], .floating_nodes[]) | select(.type=="con" or .type=="floating_con")' > /dev/null || swaymsg -t get_tree | jq --argjson ws "$((current + 1))" -e '.nodes[].nodes[] | select(.type=="workspace" and .num==$ws) | recurse(.nodes[], .floating_nodes[]) | select(.type=="con" or .type=="floating_con")' > /dev/null); then
+    # Go right, creating windows if not currently in empty workspace or if next workspace to left not empty
     swaymsg workspace $((current + 1))
 fi
